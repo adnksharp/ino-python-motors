a8653d02ba8a1048e55f3620ef3174305406583b
akey <darkmark404@protonmail.com>
2025-11-14 14:25:30 -0700

    test: init server service

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/enc_status', methods=['POST'])
+def receive_data():
+    if request.is_json:
+        data = request.get_json()
+        print(f">> {data}")
+
+        return jsonify({"status": "success", "message": "Data received"}), 200
+    else:
+        return jsonify({"status": "error", "message": "Content-Type must be application/json"}), 400
+
+
+if __name__ == '__main__':
+    app.run(host='10.42.0.1')
